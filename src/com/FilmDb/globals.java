c9c33270aed97eb4303df135b24eaa0c489f31a9
Stijn Delarbre <stijn.delarbre1@gmail.com>
2011-06-09 21:41:33 +0200

    Prepared globals for swipe events on home screen

--- src/com/FilmDb/globals.java
+++ src/com/FilmDb/globals.java
@@ -7,31 +7,98 @@ package com.FilmDb;
 public class globals {
 
 
-    public static final int ACTIVITY_CREATE=0;
-    public static final int ACTIVITY_SHOW=1;
-    public static final int ACTIVITY_TRAILER=2;
-    public static boolean sortTitle = true;
-    private static globals instance;
+	public static enum GenreEnum
+	{
+		ALL,
+		ACTION,
+		THRILLER,
+		HORROR,
+		ROMANCE,
+		DRAMA,
+		COMEDY
+	}
 
-    static {
-        instance = new globals();
-    }
+	public static final int ACTIVITY_CREATE=0;
+	public static final int ACTIVITY_SHOW=1;
+	public static final int ACTIVITY_TRAILER=2;
+	public static boolean sortTitle = true;
+	public static GenreEnum currentGenre = GenreEnum.ALL;
+	private static globals instance;
 
-    private globals() {
-    }
+	static {
+		instance = new globals();
+	}
 
-    public static globals getInstance() {
-        return globals.instance;
-    }
-    
-    public static boolean sortByTitle()
-    {
-    	return sortTitle;
-    }
-    
-    public static void toggleSort()
-    {
-    	sortTitle = !sortTitle;
-    }
+	private globals() {
+	}
 
+	public static globals getInstance() {
+		return globals.instance;
+	}
+
+	public static boolean sortByTitle()
+	{
+		return sortTitle;
+	}
+
+	public static void toggleSort()
+	{
+		sortTitle = !sortTitle;
+	}
+
+	public static GenreEnum nextGenre()
+	{
+		switch(currentGenre)
+		{
+		case ALL:
+			currentGenre = GenreEnum.ACTION;
+			break;
+		case ACTION:
+			currentGenre = GenreEnum.THRILLER;
+			break;
+		case THRILLER:
+			currentGenre = GenreEnum.HORROR;
+			break;
+		case HORROR:
+			currentGenre = GenreEnum.ROMANCE;
+			break;
+		case ROMANCE:
+			currentGenre = GenreEnum.DRAMA;
+			break;
+		case DRAMA:
+			currentGenre = GenreEnum.COMEDY;
+			break;
+		default:
+			currentGenre = GenreEnum.ALL;
+		}
+		return currentGenre;
+	}
+	
+	public static GenreEnum previousGenre()
+	{
+		switch(currentGenre)
+		{
+		case ALL:
+			currentGenre = GenreEnum.COMEDY;
+			break;
+		case COMEDY:
+			currentGenre = GenreEnum.DRAMA;
+			break;
+		case DRAMA:
+			currentGenre = GenreEnum.ROMANCE;
+			break;
+		case ROMANCE:
+			currentGenre = GenreEnum.HORROR;
+			break;
+		case HORROR:
+			currentGenre = GenreEnum.THRILLER;
+			break;
+		case THRILLER:
+			currentGenre = GenreEnum.ACTION;
+			break;
+		default:
+			currentGenre = GenreEnum.ALL;
+		}
+		return currentGenre;
+	}
 }
